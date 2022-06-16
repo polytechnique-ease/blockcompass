8855f3f991f5596041f3d1114228584afa01b3f3
wejdene.haouari <haouari.wejdene1@gmail.com>
2022-06-16 14:47:37 -0400

    change front config

--- front/src/environments/environment.prod.ts
+++ front/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://3.144.201.179:3001',
-  SOCKET_ENDPOINT: 'http://3.144.201.179:3001/api'
+  apiUrl: 'http://localhost:3001',
+  SOCKET_ENDPOINT: 'http://localhost:3001/api'
 };
 
